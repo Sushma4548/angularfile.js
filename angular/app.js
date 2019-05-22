2072c901ecc0bd91248dedfce6f622f83d280845
sushmareddy <sushmarani.as72>
2019-05-22 16:59:35 -0500

    commited

--- angular/app.js
+++
@@ -1,6 +0,0 @@
-angular.module("myApp", [])
-.controller("myFirstController", ['$scope', function ($scope) {
-    console.log("this is a form controller");
-$scope.Aleti = "hi";
-$scope.hello = "hello world";
-}]);
